acc9a6c6dca8c37832e8ca2c66f1d61da7e195e0
MihirM <Mihir9874@gmail.com>
2019-10-02 22:59:46 +0530

    ip track

--- Modern/views.py
+++ Modern/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from services.models import shortenedurl,ent_url_data
 from ipware import get_client_ip
 from ipdata import ipdata
+from datetime import datetime
 
 ipdata = ipdata.IPData('6e5377b93bec490ff6500b084cba36f1c025a0c9057215a082636d3d')
 
@@ -12,8 +13,8 @@ def home(request):
 def shurl(request,sh_id):
     if shortenedurl.objects.filter(sh_url=sh_id,is_ent_user=True).exists():
         client_ip, is_routable = get_client_ip(request)
-        response = ipdata.lookup(client_ip, fields=['continent_name','country_name','region','city','postal','time_zone'])
-        data = ent_url_data(sh_url=sh_id,continent=response['continent_name'],state_region=response['region'],country=response['country_name'],city=response['city'],postal_code=response['postal'],date_time=response['time_zone']['current_time'])
+        response = ipdata.lookup(client_ip, fields=['continent_name','country_name','region','city','postal'])
+        data = ent_url_data(sh_url=sh_id,continent=response['continent_name'],state_region=response['region'],country=response['country_name'],city=response['city'],postal_code=response['postal'],date_time=datetime.today)
         data.save()
         url_data=shortenedurl.objects.get(sh_url=sh_id,is_ent_user=True)
         return redirect(url_data.org_url)
